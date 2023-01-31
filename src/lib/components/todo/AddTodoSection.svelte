99b902cd2750daac02969bed482687e4eff3fe56
Hunter Garrett <hunter@hunterbecton.com>
2023-01-31 15:11:36 -0500

    AddTodoSection

---
+++ src/lib/components/todo/AddTodoSection.svelte
@@ -0,0 +1,34 @@
+<script>
+	import { addTodo } from '../../store/TodoStore';
+</script>
+
+<section class="flex justify-between">
+	<h3 class="text-xl font-bold text-gray-three">Checklist</h3>
+	<button
+		class="group flex items-center rounded-md border-cream-four bg-green-one px-6 py-3 text-lg font-semibold text-gray-five outline outline-2 outline-offset-2 outline-green-one hover:text-green-five focus-visible:text-green-five focus-visible:outline-green-five"
+		type="button"
+		on:click={addTodo}
+	>
+		Add Task
+		<svg
+			class="ml-3 h-4 w-4 text-gray-five group-hover:text-green-five group-focus-visible:text-green-five"
+			width="32"
+			height="32"
+			viewBox="0 0 32 32"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<g clip-path="url(#clip0_35_391)">
+				<path
+					d="M14.4 14.4V8H17.6V14.4H24V17.6H17.6V24H14.4V17.6H8V14.4H14.4ZM16 32C7.1632 32 0 24.8368 0 16C0 7.1632 7.1632 0 16 0C24.8368 0 32 7.1632 32 16C32 24.8368 24.8368 32 16 32ZM16 28.8C19.3948 28.8 22.6505 27.4514 25.051 25.051C27.4514 22.6505 28.8 19.3948 28.8 16C28.8 12.6052 27.4514 9.3495 25.051 6.94903C22.6505 4.54857 19.3948 3.2 16 3.2C12.6052 3.2 9.3495 4.54857 6.94903 6.94903C4.54857 9.3495 3.2 12.6052 3.2 16C3.2 19.3948 4.54857 22.6505 6.94903 25.051C9.3495 27.4514 12.6052 28.8 16 28.8V28.8Z"
+					fill="currentColor"
+				/>
+			</g>
+			<defs>
+				<clipPath id="clip0_35_391">
+					<rect width="32" height="32" fill="white" />
+				</clipPath>
+			</defs>
+		</svg>
+	</button>
+</section>
